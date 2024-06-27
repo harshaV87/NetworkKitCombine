ce640d4183db034306060d0fafe62bfcd173ad41
Harsha <harsha.87b@gmail.com>
2024-06-26 23:09:45 -0500

    URL Components

--- Sources/NetworkKit/URLBuilder.swift
+++ Sources/NetworkKit/URLBuilder.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+
+
+// Defining the interface
+
+public protocol URLBuilderInterface {
+    func buildURL() -> URL?
+}
+
+// Struct
+
+public struct URLComponentsBuilder: URLBuilderInterface {
+   // URL Components
+    var scheme: String 
+    var host: String
+    var path: String
+    var queryItems: [URLQueryItem]?
+    
+    public init(scheme: String, host: String, path: String, queryItems: [URLQueryItem]? = nil) {
+        self.scheme = scheme
+        self.host = host
+        self.path = path
+        self.queryItems = queryItems
+    }
+    
+    public func buildURL() -> URL? {
+        // defining components
+        var components = URLComponents()
+        components.queryItems = queryItems
+        components.host = host
+        components.scheme = scheme
+        components.path = path
+        return components.url
+    }
+}
--- Tests/NetworkKitTests/NetworkKitTests.swift
+++ Tests/NetworkKitTests/NetworkKitTests.swift
@@ -2,11 +2,30 @@ import XCTest
 @testable import NetworkKit
 
 final class NetworkKitTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    
+    var sut: URLBuilderInterface!
+    
+    override func setUp() {
+        sut = MockURLBuilder()
     }
+    
+    func testURLComponentsBuilder() {
+        let queryItemI = URLQueryItem(name: "i", value: "tt3896198")
+        let queryItemApiKey = URLQueryItem(name: "apikey", value: "d9482b5b")
+        let testURL = URLComponentsBuilder(scheme: "https", host: "www.omdbapi.com", path: "/", queryItems: [queryItemI, queryItemApiKey])
+        XCTAssertEqual(testURL.buildURL(), sut.buildURL())
+    }
+    
+    override func tearDown() {
+        sut = nil
+    }
+}
+
+
+class MockURLBuilder: URLBuilderInterface {
+    func buildURL() -> URL? {
+        return URL(string: "https://www.omdbapi.com/?i=tt3896198&apikey=d9482b5b")
+    }
+    
+    
 }
