3183213bd4f71006fcb13160d2aac4bca34f7978
Harsha <harsha.87b@gmail.com>
2024-06-28 11:20:43 -0500

    Reactive networking with unit tests

---
+++ Sources/NetworkKit/NetworkError.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by BV Harsha on 2024-06-26.
+//
+
+import Foundation
---
+++ Tests/NetworkKitTests/MovieTestObject.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by BV Harsha on 2024-06-27.
+//
+
+import Foundation
